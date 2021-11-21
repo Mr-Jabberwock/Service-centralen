d1c1eb330cc72dec6c63def0686b377494b9e6af
Mr-Jabberwock <djohnsen143@gmail.com>
2021-11-21 10:24:13 +0100

    Started on the Company page

---
+++ service-centralen/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+   <div>
+       <div class="header">
+           <div class="logo">LOGO</div>
+           <div>Firmaer</div>
+           <div>Statestikker</div>
+           <div>Oversigt</div>
+       </div>
+   </div>
+</template>
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+<style lang="scss">
+.header{
+    width: 100%;
+    height: 50px;
+    background-color: black;
+    opacity: 70%;
+
+    display: flex;
+
+    .logo{
+        margin-left: 10px;
+        margin-top: 0;
+    }
+
+    
+}
+
+    @media only screen and (max-width: 786){
+
+    }
+</style>
---
+++ service-centralen/src/views/Companies.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+    <Header />
+    <ul :style="gridStyle" class="company">
+        <li v-for="company in companies" :key="company" class="company__details">
+            <p>Comapny {{company.company}}</p>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+import Header from "../components/Header.vue"
+export default{
+   name: 'Companies',
+   data(){
+       return{
+           companies: [
+               {company: "comapny 1"},
+               {company: "comapny 2"},
+               {company: "comapny 3"},
+               {company: "comapny 4"},
+               {company: "comapny 5"},
+               {company: "comapny 6"},
+               {company: "comapny 7"},
+               {company: "comapny 8"}
+           ]
+       }
+   },
+   components: {
+      Header
+   }
+}
+</script>
+
+<style lang="scss">
+
+.company{
+    display: flex;
+    flex-direction: column;
+    padding-left: 0px;
+    .company__details{
+        width: 80%;
+        height: 50px;
+        margin-top: 20px;
+        margin-left: 10%;
+
+        display: flex;
+        background-color: red;
+    }
+}
+
+@media only screen and (min-width: 1000px){
+    .company{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 50px 0px;
+        
+    }
+
+}
+
+</style>
