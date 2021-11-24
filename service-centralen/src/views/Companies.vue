0e8bcc5c5bc5c042782ba29182309413ab73f3d4
Mr-Jabberwock <djohnsen143@gmail.com>
2021-11-24 09:48:58 +0100

    Year filter works.

--- service-centralen/src/views/Companies.vue
+++ service-centralen/src/views/Companies.vue
@@ -2,17 +2,35 @@
 <div>
     <Header />
     <div>
-        <input type="file" @change="uploadedFile" />
+        <input type="file" @change="uploadFile" />
+        <select v-model="filter" @change="filterResults">
+            <option>Største</option>
+            <option>Mindste</option>
+            <option>Røde</option>
+        </select>
+        <select v-model="searchYear" @change="sortByYear">
+            <option>2021</option>
+            <option>2020</option>
+            <option>2019</option>
+            <option>2018</option>
+            <option>2017</option>
+            <option>2016</option>
+        </select>
     </div>
-    <ul :style="gridStyle" class="company">
-        <li v-for="company in toBeShown" :key="company" class="company__details">
-            <p>Comapny {{company.company}}</p>
-            <p> {{company.amount}}</p>
+    <ul  class="company">
+        <li v-for="company in toBeShown" :key="company.invoice" >
+            <div class="company__details"
+                 :class="company.price > company.year2 ? 'yellow' : 'green'"
+            >
+                <p>Comapny {{company.address}}  </p>
+                <p> {{company.price}}</p>
+            </div>
         </li>
     </ul>
     <div>
         <button class="load-button" @click="increasePage">Indlæs 8 mere</button>
     </div>
+
 </div>
 </template>
 
@@ -23,9 +41,13 @@ export default{
    name: 'Companies',
    data(){
        return{
+           invoices: [],
            companies: [],
+           filteredCompanies: [],
            currentPage: 1,
-           file: ""
+           file: "",
+           filter: "",
+           searchYear: 2021
        }
    },
    components: {
@@ -40,32 +62,88 @@ export default{
        increasePage(){
            this.currentPage++
        },
-       uploadedFile(event){
+       uploadFile(event){
            this.file = event.target.files[0]
            const reader = new FileReader();
            reader.onload = (e) => {
-                /* Parse data */
-                const bstr = e.target.result;
-                const wb = XLSX.read(bstr, { type: 'binary' });
+                /* Parse data. XLSX is an imported library */
+                const binaryStr = e.target.result;
+                const wordBook = XLSX.read(binaryStr, { type: 'binary' });
                 
                 /* Get first worksheet */
-                const wsname = wb.SheetNames[0];
-                const ws = wb.Sheets[wsname];
+                const workSheetNames = wordBook.SheetNames[0];
+                const workSheet = wordBook.Sheets[workSheetNames];
                 /* Convert array of arrays */
-                const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+                const data = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
                 
                 var objs = data.map(function(x) { 
                     return { 
-                        company: x[0], 
-                        amount: x[1] 
+                        invoice: x[0], 
+                        date: x[1],
+                        companyId: x[2], 
+                        address: x[3],
+                        price: x[4] 
                     }; 
                 });
-                console.log(objs);
-                this.companies = objs
+                
+                this.invoices = objs;
+
+                const result = [];
+                for(var i = 0; i < this.invoices.length; i++){
+                    var el = this.invoices[i];
+                    if(!result.some(invoice => invoice.companyId === el.companyId)){
+                        result.push({ invoice: el.invoice, date: el.date, companyId: el.companyId, address: el.address, price: 0, year2: 0, year1: 0 })
+                    }
+                    
+                    const year = el.date.toString().substring(6,10);
+                    const elementPos = result.map((x) => {return x.companyId; }).indexOf(el.companyId);
+                    if(result.some(invoice => invoice.companyId === el.companyId)){
+                        result[elementPos].price += el.price
+
+                        if(this.searchYear -1 === parseInt(year)){
+                            result[elementPos].year2 += el.price
+                        }
+
+                        if(this.searchYear -2 === parseInt(year)){
+                            result[elementPos].year1 += el.price
+                        }
+                    }
+                }
+
+                console.log(result)
+
+                this.companies = result   
 
             }  
            reader.readAsBinaryString(this.file);
         
+       },
+       sortByYear(){
+
+            const result = [];
+
+             for(var i = 0; i < this.invoices.length; i++){
+                var el = this.invoices[i];
+                if(!result.some(invoice => invoice.companyId === el.companyId)){
+                    result.push({ invoice: el.invoice, date: el.date, companyId: el.companyId, address: el.address, price: 0 })
+                }
+                    
+                const year = el.date.toString().substring(6,10);
+                const elementPos = result.map((x) => {return x.companyId; }).indexOf(el.companyId);
+                if(result.some(invoice => invoice.companyId === el.companyId)){
+                    if(this.searchYear == year){
+                        result[elementPos].price += el.price
+                    }
+                }        
+            }
+
+            this.companies = result 
+
+            
+       },
+
+       filterResults(){
+         
        }
    }
 }
@@ -84,8 +162,16 @@ export default{
         margin-left: 10%;
 
         display: flex;
-        background-color: red;
+        
     }
+}
+
+.green{
+    background-color: green;
+}
+
+.yellow{
+    background-color: yellow;
 }
 
 .load-button{
