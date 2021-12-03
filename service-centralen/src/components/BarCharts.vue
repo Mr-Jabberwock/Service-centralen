8743b73962b4adbbf1037899969ab2298a0818d7
Mr-Jabberwock <djohnsen143@gmail.com>
2021-12-03 19:21:55 +0100

    bar chart draft

--- service-centralen/src/components/BarCharts.vue
+++ service-centralen/src/components/BarCharts.vue
@@ -1,5 +1,32 @@
 <template>
- <div><input type="text" v-model="searchNumber" v-on:keyup.enter="computeByCustomerNumber "/></div>
+ <div>
+     {{total}}
+     <input type="text" v-model="searchNumber" v-on:keyup.enter="computeByCustomerNumber "/>
+     <div class="barchart">
+        
+            <ul class="barchart__sidebar">
+                    <li style="height: 20%">{{total}}</li>
+                    <li style="height: 20%">{{total / 100 * 80}}</li>
+                    <li style="height: 20%">{{total / 100 * 60}}</li>
+                    <li style="height: 20%">{{total / 100 * 40}}</li>
+                    <li style="height: 20%">{{total / 100 * 20}}</li>
+                    <li style="height: 0%">0</li>
+
+            </ul>
+             <ul class="barcontainer">
+                 
+                 <div class="barContainer__bars"  v-for="element in customerStatistics" :key="element.year">
+                     <li class='bar' :style="{height: element.percentage + '%'}" scope="row" > 
+                        <div >{{element.price}}</div>
+                     </li>
+                     <li class="year">
+                         <div>{{element.year}}</div>
+                     </li>
+                 </div>
+             </ul>
+    
+     </div>
+ </div>
 </template>
 
 <script>
@@ -8,37 +35,54 @@ export default {
     data(){
         return{
           invoices: this.$store.getters.getCompanyPurchases,
-          searchNumber: ""
+          searchNumber: "",
+          customerStatistic: "",
+          total: 0
         }
 
     },
     computed:{
         customerStatistics(){
-            return this.invoices
+            return this.customerStatistic
         }
     },
     methods: {
         computeByCustomerNumber(){
             const result = [];
+            let total = 0;
             for(var i = 0; i < this.invoices.length; i++){
                 var el = this.invoices[i];
                 const year = el.date.toString().trim().substring(6,10);
                 if(el.companyId == this.searchNumber){
                     if(!result.some(invoice => invoice.year == year)){
-                        result.push({year: year, price: el.price })
+                        result.push({year: year, price: el.price, percentage: 0 })
                     }
                     const elementPos = result.map((x) => {return x.year; }).indexOf(year);
 
                     if(result.some(invoice => invoice.year == year)){
                         result[elementPos].price += el.price
-
+                        total += el.price;
                     }
 
 
                 }
+
+                this.total = total;
+
+                if(i == this.invoices.length -1){
+                    
+                    for(var j = 0; j < result.length; j++){
+                        var percentage = (result[j].price / total) * 100;
+                        result[j].percentage = percentage.toFixed(0)
+                    }
+                }
             }
-            console.log(result)
+            this.customerStatistic = result
         }
     }
 }
 </script>
+
+<style lang="scss">
+@import "../assets/styles/barCharts";
+</style>
