a85ea39e639c81e988086d5da40e36eada3d5b49
Ibsylong <luanbanguyen94@gmail.com>
2021-12-03 20:59:10 +0100

    03-12-2021 cirkel diagram
    03-12-2021 cirkel diagram

---
+++ service-centralen/src/PieChart.js
@@ -0,0 +1,9 @@
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  props: ['chartdata', 'options'],
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
