c4daf172492126091442e962dfb8be559e77418e
Ibsylong <luanbanguyen94@gmail.com>
2021-12-21 00:47:28 +0100

    Email modal
    Email modal

--- service-centralen/src/components/CompanyDetails.vue
+++ service-centralen/src/components/CompanyDetails.vue
@@ -34,14 +34,61 @@
                 </div>
             </section>
         </div>
+        <button @click="showEmail = true">Send offer</button>
+        <transition name="email-fade" appear>
+            <div class="email-overlay" 
+                v-if="showEmail" 
+                @click="showEmail = false"></div>
+        </transition>
+        <transition name="email-pop" appear>
+            <div class="email-modal" 
+                v-if="showEmail" >              
+                    <form>
+                    <label for="target-email-id">Target Email ID</label>
+                    <input
+                        name="target-email-id"
+                        placeholder="Target Email ID"
+                        type="email"
+                        v-model="emailId"
+                        v-on:keyup="updateOutputUrl"
+                    />
+                    <label for="subject">Subject</label>
+                    <input
+                        name="subject"
+                        placeholder="Subject"
+                        type="text"
+                        v-model="email.subject"
+                        v-on:keyup="updateOutputUrl"
+                    />
+                    <label for="body">Body</label>
+                    <textarea
+                        name="body"
+                        placeholder="Body"
+                        type="text"
+                        v-model="email.body"
+                        v-on:keyup="updateOutputUrl"
+                    ></textarea>
+                    <input type="hidden" id="final-link-to-copy" :value="outputUrl" />
+                    </form>
+                    <button @click="sendEmail">Send email</button>
+                    <button @click="showEmail = false">Cancel</button>
+                </div>
+        </transition>
     </div>
 </template>
 <script>
+
 export default {
     name:'CompanyDetails',
     data(){
         return{
-
+            outputUrl: "Type something",
+            emailId: "",
+            email: {
+                subject: "",
+                body: ""
+            },
+            showEmail: false
         }
     },
     computed: {
@@ -52,7 +99,26 @@ export default {
     methods: {
         closeModal(){
             this.$emit("closeCompanyDetails");
+        },
+        sendEmail(){
+            window.open(this.outputUrl);
+        },
+        updateOutputUrl() {
+            this.outputUrl = "mailto:" + this.emailId;
+            const emailKeys = Object.keys(this.email);
+            const remaining = emailKeys.filter(
+                (key) => this.email[key].trim().length > 0
+            );
+            if (remaining.length > 0) {
+                this.outputUrl += "?";
+            }
+            this.outputUrl += remaining
+                .map((key) => `${key}=${encodeURI(this.email[key].trim())}`)
+                .join("&");
         }
+    },
+    created() {
+        this.updateOutputUrl();
     }
 }
 </script>
